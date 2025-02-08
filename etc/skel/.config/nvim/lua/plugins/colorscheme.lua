977dff902c86bb37e41f894a6aed14c37d6940e7
DarkXero-dev <steve@techxero.com>
2025-02-08 09:14:26 +0200

    Updated nVim config

--- etc/skel/.config/nvim/lua/plugins/colorscheme.lua
+++ etc/skel/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,8 +1,8 @@
 return 
 	{
-        "EdenEast/nightfox.nvim",
+        "Mofiqul/dracula.nvim",
         config = function()
-           vim.cmd.colorscheme("nightfox")
+           vim.cmd.colorscheme("dracula")
         end,
     }	
  
--- etc/skel/.config/nvim/lua/plugins/markdown.lua
+++
@@ -1,9 +0,0 @@
-return 
-{
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    ft = { "markdown" },
-    build = function()
-        vim.cmd("call mkdp#util#install()")
-    end,
-}
